8dcf8b4c59bf37bdefe43341e2f1da44fd29d75d
VincentShao32 <yunbo2016@gmail.com>
2026-04-15 14:18:39 -0700

    generate script

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 cd /gpfs/scrubbed/yunbos/video_datasets/molmospaces/molmospaces && \
-MUJOCO_GL=egl PYOPENGL_PLATFORM=egl \
-python -m molmo_spaces.data_generation.main FrankaPickPointTrackDebug
+MUJOCO_GL=egl PYOPENGL_PLATFORM=egl MUJOCO_INSTALL_DIR=/gpfs/scrubbed/yunbos/video_datasets/molmospaces/assets/ \
+python -m molmo_spaces.data_generation.main FrankaPickDroidDataGenConfig
